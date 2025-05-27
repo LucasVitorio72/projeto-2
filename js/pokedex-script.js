77981ad7b5a2384da57fab4314145ff8f3c2ec9b
IHeroTank <megaxbrelsword@gmail.com>
2025-05-26 23:22:02 -0300

    Add files via upload

--- js/pokedex-script.js
+++ js/pokedex-script.js
@@ -205,6 +205,7 @@ inputNext?.addEventListener('click', () => {
 // Inicializa com o primeiro Pokémon
 renderPokemon(searchPokemon);
 
+//Tra
 document.addEventListener('DOMContentLoaded', function () {
     const currentPage = window.location.pathname.split('/').pop() || 'index.html';
     const menuLinks = document.querySelectorAll('.menu-link');
